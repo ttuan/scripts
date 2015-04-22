b779701629c4a35b74d63c544118be5802708901
tuan <tuantv.nhnd@gmail.com>
2015-04-22 20:23:06 +0700

    Add send email tool

---
+++ sentEmail.py
@@ -0,0 +1,25 @@
+import sys,smtplib
+from email.mime.text import MIMEText
+
+
+fp = open('desktop.ini', 'rb')
+message = fp.read()
+fp.close()
+
+to = 'tuantv_57@vnu.edu.vn'
+gmail_user = 'tikitaka.yugi@gmail.com'
+gmail_pwd = 'Haianhanh1'
+
+smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+smtpserver.ehlo
+smtpserver.login(gmail_user, gmail_pwd)
+
+header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:send data of file desktop.ini \n'
+print header
+msg = header + message
+
+smtpserver.sendmail(gmail_user, to, msg)
+print 'done!'
+smtpserver.close()
