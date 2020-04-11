bec0106b287c80460c8598858526979e0bc6ee82
ttuan <tuantv.nhnd@gmail.com>
2020-04-11 15:29:15 +0700

    Update url regex for matching issues page with params

--- tampermonkey/redmine_remove_sidebar.js
+++ tampermonkey/redmine_remove_sidebar.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://dev.sun-asterisk.com/projects/trill-dely/issues
+// @match        https://dev.sun-asterisk.com/projects/trill-dely/issues*
 // @grant        none
 // ==/UserScript==
 
