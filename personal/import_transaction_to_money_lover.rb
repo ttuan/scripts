1c43dfd04552ecd642a34a7530d3f22797e588f9
ttuan <tuantv.nhnd@gmail.com>
2023-09-28 00:08:19 +0700

    Fix script import transactions to money lover

--- personal/import_transaction_to_money_lover.rb
+++ personal/import_transaction_to_money_lover.rb
@@ -3,27 +3,29 @@
 # Read file and import to Money Lover with this command:
 # ruby import_transaction_to_money_lover.rb transactions.xlsx
 
-require "roo"
-require "watir"
 require "faraday"
+require "roo"
 
 class MoneyLoverOperator
   attr_accessor :access_token
-
-  BASE_URL = 'https://web.moneylover.me'
 
   ML_ACCOUNT_EMAIL = ENV["ML_ACCOUNT_EMAIL"]
   ML_ACCOUNT_PASSWORD = ENV["ML_ACCOUNT_PASSWORD"]
   ML_WALLET_ID = ENV["ML_WALLET_ID"]
 
-  def bulk_import transactions
-    transactions_path = "/api/transaction/add"
-    url = BASE_URL + transactions_path
-    headers = {
-      "Authorization" => "AuthJWT #{fetch_token}",
-      "Content-Type" => "application/json"
-    }
+  DEFAULT_HEADERS = {
+    "client" => "lzruIoplt7I9",
+    "appversion" => "7021",
+    "apiVersion" => "4",
+    "User-Agent" => "MoneyLoverFree/7.21.2 (iPhone; iOS 17.0; Scale/3.00)",
+  }
 
+  def bulk_import transactions
+    transaction_url = "https://revoapi.moneylover.me/api/sync/push/transaction/v2"
+    headers = {
+      "Authorization" => "Bearer #{fetch_token}",
+      "Content-Type" => "application/json",
+    }.merge(DEFAULT_HEADERS)
     ignored_transactions = []
 
     puts "=========== Import to Money Lover ==========="
@@ -37,31 +39,42 @@ class MoneyLoverOperator
       if category.nil?
         ignored_transactions << transaction
       else
+        data = JSON.dump({
+          "d" => [
+            {
+              "lo"=>0, # Longitude
+              "ad"=>"", # Address
+              "la"=>0, # Latitude
+              "oc"=>"",
+              "a"=> transaction[:amount], # amount
+              "ac"=> ML_WALLET_ID, # wallet id
+              "pi"=>"",
+              "v"=>0,
+              "c"=> category[:_id], # category id
+              "gid"=> SecureRandom.uuid, # Transaction id
+              "mr"=>false,
+              "er"=>false, # exclude report
+              "dd"=> transaction[:displayDate], # display date
+              "rd"=>0,
+              "n"=> note, # note
+              "f"=>1,
+              "im"=>[] # image
+            }
+          ]
+        })
         body = JSON.dump({
-          "with": [],
-          "account": ML_WALLET_ID,
-          "category": category[:_id],
-          "amount": transaction[:amount],
-          "note": note,
-          "displayDate": transaction[:displayDate],
-          "event": "",
-          "exclude_report": false,
-          "longtitude": 0,
-          "latitude": 0,
-          "addressName": "",
-          "addressDetails": "",
-          "addressIcon": "",
-          "image": ""
+          "pl" => 2,
+          "data" => data,
+          "av" => 7021,
         })
 
-        response = Faraday.post(url, body, headers)
+        response = Faraday.post(transaction_url, body, headers)
         data = JSON.parse(response.body, symbolize_names: true)
 
-        ignored_transactions << transaction unless data[:error] == 0
+        ignored_transactions << transaction if data[:failedItems].any?
       end
     end
 
-    # Write ignored transactions to file csv
     return if ignored_transactions.empty?
 
     File.open("ignored_transactions.csv", "w") do |f|
@@ -71,45 +84,62 @@ class MoneyLoverOperator
     end
   end
 
+
   private
   def categories
     return @categories if @categories
 
-    categories_path = "/api/category/list-all"
-    url = BASE_URL + categories_path
+    category_url = "https://revoapi.moneylover.me/api/sync/pull/category/v2"
     headers = {
-      Authorization: "AuthJWT #{fetch_token}"
-    }
+      "Authorization": "Bearer #{fetch_token}",
+      "Content-Type" => "application/json",
+    }.merge(DEFAULT_HEADERS)
+    all_categories = []
+    skip = 0
+    while true
+      body = JSON.dump({
+        "last_update" => 0,
+        "limit" => 80,
+        "skip" => skip
+      })
+      response = Faraday.post(category_url, body, headers)
+      response_data = JSON.parse(response.body, symbolize_names: true)
+      all_categories += response_data[:data]
 
-    response = Faraday.post(url, {}, headers)
-    all_categories = JSON.parse(response.body, symbolize_names: true)[:data]
-    @categories = all_categories.select { |category| category[:account] == ML_WALLET_ID }
+      break if response_data[:data].size == 0
+      skip += 80
+    end
+
+    @categories = all_categories.select { |category| category[:account][:_id] == ML_WALLET_ID }
   end
 
   def fetch_token
     return @access_token if @access_token
 
-    browser = Watir::Browser.new :chrome, options: {args: %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222]}
-    service_url = "https://web.moneylover.me/"
-    browser.goto service_url
-
-    # Wait util redirect success and get token
-    sleep(5)
-    token = browser.url.split("token=")[1].split("&")[0]
-
+    # Request token
     headers = {
-      Authorization: "Bearer #{token}",
-      client: "kHiZbFQOw5LV"
+      "Authorization": "Basic bHpydUlvcGx0N0k5OllQcURIZ0kzbVJiMVlkdjRmbzBUcEMyUnc3ZFZHcA==",
+      "Content-Type" => "application/x-www-form-urlencoded"
+    }.merge(DEFAULT_HEADERS)
+
+    response = Faraday.post("https://oauth.moneylover.me/request-token", {}, headers)
+    data = JSON.parse(response.body, symbolize_names: true)
+    request_token = data[:request_token]
+
+    # Request access_token
+    headers = {
+      Authorization: "Bearer #{request_token}",
+      client: "lzruIoplt7I9",
     }
-
-    oauth_url = "https://oauth.moneylover.me/token"
-    body = {"email": ML_ACCOUNT_EMAIL, "password": ML_ACCOUNT_PASSWORD }
-
-    response = Faraday.post(oauth_url, body, headers)
+    body = {
+      "email": ML_ACCOUNT_EMAIL,
+      "password": ML_ACCOUNT_PASSWORD,
+      "purchased": true,
+      "grant_type": "password"
+    }
+    response = Faraday.post("https://oauth.moneylover.me/token", body, headers)
     data = JSON.parse(response.body, symbolize_names: true)
     @access_token = data[:access_token]
-
-    @access_token
   end
 end
 
